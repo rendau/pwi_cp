713468490b34ee1d8577e3f422396725b1807231
neruad <dauren.nurlanov@gmail.com>
2017-01-18 03:07:40 +0600

    finish i18n

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import Polyglot from 'node-polyglot'
 import translations from './i18n'
 
-var API_URL_PREFIX = 'http://192.168.1.33:8888/api/user';
+var API_URL_PREFIX = '/api/user';
 var pgt = new Polyglot()
 
 class App extends React.Component {
@@ -14,6 +14,7 @@ class App extends React.Component {
     this.state = {
       loading: true,
       hasAccess: false,
+      hideBody: false,
       formStep: 1,
       step1PhoneValue: '',
       step2CodeValue: '',
@@ -31,16 +32,24 @@ class App extends React.Component {
   }
   render() {
     return <div id="container">
+      { this.renderLangBar() }
       <div id="logo"></div>
       { this.renderBody() }
       { this.renderMsg() }
     </div>;
   }
+  renderLangBar() {
+    return <ul className="langbar">
+      <li className={ (this.state.locale=='en') ? 'selected' : '' } onClick={this.onLangClick.bind(this, 'en')}>English</li>
+      <li className={ (this.state.locale=='ru') ? 'selected' : '' } onClick={this.onLangClick.bind(this, 'ru')}>Русский</li>
+      <li className={ (this.state.locale=='kz') ? 'selected' : '' } onClick={this.onLangClick.bind(this, 'kz')}>Қазақша</li>
+    </ul>
+  }
   renderBody() {
     if(this.state.loading) {
       return <img src={PreloaderFile} />
     }
-    if(this.state.loading || this.state.hasAccess) return;
+    if(this.state.hideBody || this.state.loading || this.state.hasAccess) return;
     return <div>
       <div className="header" dangerouslySetInnerHTML={{__html: pgt.t('header_text')}}></div>
       { this.renderForm() }
@@ -55,27 +64,27 @@ class App extends React.Component {
             ref={input => input && input.focus()} />
         </div>
         <div className="action">
-          <a className="bt1" onClick={this.onSendCodeClick.bind(this)}>Получить код</a>
-          <a className="bt2" onClick={this.onHasCodeClick.bind(this)}>уже есть код</a>
+          <a className="bt1" onClick={this.onSendCodeClick.bind(this)}>{pgt.t('step1_bt1')}</a>
+          <a className="bt2" onClick={this.onHasCodeClick.bind(this)}>{pgt.t('step1_bt2')}</a>
         </div>
       </div>;
     } else if(this.state.formStep == 2) {
       return <div className="step">
         <div className="form">
-          <div className="label">Код:&nbsp;</div><input type="text" placeholder="6385" maxLength="10"
+          <div className="label">{pgt.t('step2_input_label')}:&nbsp;</div><input type="text" placeholder="6385" maxLength="10"
             value={this.state.step2CodeValue} onChange={this.updateStep2CodeValue.bind(this)}
             ref={input => input && input.focus()} />
         </div>
         <div className="action">
-          <a className="bt1" onClick={this.onRegisterClick.bind(this)}>Подтвердить</a>
-          <a className="bt2" onClick={this.onHasntCodeClick.bind(this)}>получить код</a>
+          <a className="bt1" onClick={this.onRegisterClick.bind(this)}>{pgt.t('step2_bt1')}</a>
+          <a className="bt2" onClick={this.onHasntCodeClick.bind(this)}>{pgt.t('step2_bt2')}</a>
         </div>
       </div>;
     }
   }
   renderMsg() {
     if(!this.state.loading && this.state.msg) {
-      return <div id="msg">{this.state.msg}</div>;
+      return <div id="msg" dangerouslySetInnerHTML={{__html: pgt.t(this.state.msg)}}></div>;
     }
     return;
   }
@@ -88,7 +97,7 @@ class App extends React.Component {
       this.state.loading = false;
       if(data.has_access === true) {
         this.state.hasAccess = true;
-        this.state.msg = 'у Вас уже есть доступ в интернет';
+        this.state.msg = 'already_have_access';
         this.refreshState();
         this.redirect(data.redirect_url);
       } else {
@@ -104,10 +113,14 @@ class App extends React.Component {
   updateStep2CodeValue(e) {
     this.setStateAttr('step2CodeValue', e.target.value);
   }
+  onLangClick(code, e) {
+    pgt.replace(translations[code]);
+    this.setStateAttr('locale', code);
+  }
   onSendCodeClick(e) {
     let pn = this.state.step1PhoneValue;
   	if(!(/^[1-9]\d{10,14}$/i).test(pn)) {
-	    this.setStateAttr('msg', pgt.t('bad_phone_number_error')+', '+pgt.t('phone_format_hint'));
+	    this.setStateAttr('msg', 'bad_phone_number_error');
 	    return;
   	}
   	this.state.msg = '';
@@ -126,12 +139,14 @@ class App extends React.Component {
     }).catch(this.onFetchError.bind(this));
   }
   onHasCodeClick(e) {
-    this.setStateAttr('formStep', 2);
+    this.state.formStep = 2;
+    this.state.msg = '';
+    this.refreshState();
   }
   onRegisterClick(e) {
     let code = this.state.step2CodeValue;
   	if(!(/^[1-9]\d{2,9}$/i).test(code)) {
-	    this.setStateAttr('msg', 'Неправильный код.');
+	    this.setStateAttr('msg', 'bad_code_error');
 	    return;
   	}
     this.state.msg = '';
@@ -145,14 +160,16 @@ class App extends React.Component {
     }).then(this.onFetchResponse.bind(this)).then((data) => {
       if(data === undefined) return;
       this.state.loading = false;
-      this.state.msg = 'успешно';
+      this.state.msg = 'success';
       this.state.hasAccess = true;
       this.refreshState();
       this.redirect(data.redirect_url);
     }).catch(this.onFetchError.bind(this));
   }
   onHasntCodeClick(e) {
-    this.setStateAttr('formStep', 1);
+    this.state.formStep = 1;
+    this.state.msg = '';
+    this.refreshState();
   }
   onFetchResponse(response) {
     if(response.ok) {
@@ -164,19 +181,48 @@ class App extends React.Component {
     }
   }
   onFetchError(error) {
-    this.setStateAttr('loading', false);
-    console.log('fetch error:', error);
+    this.state.loading = false;
+    this.state.hideBody = true;
+    this.state.msg = 'system_error_ocurred';
+    this.refreshState();
   }
   handleAPIError(data) {
-    this.setStateAttr('loading', false);
-    console.log('api error:', data);
+    this.state.loading = false;
+    switch(data.error) { // every case must refresh state
+	  case "already_registered":
+      this.state.hasAccess = true;
+      this.state.msg = 'already_have_access';
+      this.refreshState();
+      this.redirect(data.redirect_url);
+  		break;
+	  case "too_frequent":
+      this.setStateAttr('msg', 'api_error_too_frequent');
+  		break;
+	  case "limit_reached":
+      this.setStateAttr('msg', 'api_error_limit_reached');
+  		break;
+	  case "sms_send_fail":
+      this.setStateAttr('msg', 'api_error_sms_send_fail');
+  		break;
+	  case "bad_sms_code":
+      this.setStateAttr('msg', 'api_error_bad_sms_code');
+  		break;
+    default:
+      this.state.hideBody = true;
+      this.state.msg = 'system_error_ocurred';
+      this.refreshState();
+      break;
+	  }
   }
   redirect(url) {
   	if(url) {
-  	  if(url.indexOf('http://') && url.indexOf('https://'))
+  	  if(url.indexOf('http://') && url.indexOf('https://')) {
   		  url = 'http://' + url;
+      }
   	  document.location.href = url;
-  	}
+  	} else {
+      document.location.href = 'http://google.com';
+    }
   }
 }
 
