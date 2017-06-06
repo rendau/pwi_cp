ed7fdc3feee791468ce1400aee24617e750acdb8
neruad <dauren.nurlanov@gmail.com>
2017-06-06 20:08:27 +0600

    move to new api

--- src/js/qpars.js
+++ src/js/qpars.js
@@ -1,20 +1,10 @@
 function getParams() {
   let result = {
-    token: '',
-    mac: '',
-    ip: '',
-    llink: '',
+    token: window.token || '',
+    mac: window.mac || '',
+    ip: window.ip || '',
+    llink: window.llink || '',
   };
-  let pars = window.location.search.substr(1).split('&')
-  if(pars) {
-    for(let i = 0; i < pars.length; i++) {
-      let p = pars[i].split('=', 2);
-      if(p.length == 1)
-        result[p[0]] = "";
-      else
-        result[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-    }
-  }
   return result;
 }
 
